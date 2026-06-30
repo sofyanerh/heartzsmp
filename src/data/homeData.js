54d69362a736d53abfda138cddad2ca5420e6afb
Sofyanerhe <rhendoursoufiane9@gmail.com>
2026-06-30 19:45:10 +0100

    Update background image URL in homeData.js

--- src/data/homeData.js
+++ src/data/homeData.js
@@ -4,7 +4,7 @@ export const homeData = {
   tagline:
     "A balanced survival experience built for fairness, creativity, and community.",
 
-  backgroundImage: "https://xgamingserver.com/img/game-images/minecraft-hero.webp", 
+  backgroundImage: "/heartzsmp/public/images/hrtliz.png", 
   // later: "/images/banner.jpg"
 
   buttons: {
